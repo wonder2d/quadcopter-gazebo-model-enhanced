66e80dfd68856b0838931435126838880f610761
wonder2d <141349715+wonder2d@users.noreply.github.com>
2022-12-11 16:24:02 +0000

    Fixes README

--- modelDev/genModels.py
+++ modelDev/genModels.py
@@ -12,3 +12,15 @@ import os
 import errno
 import sys
 import fileinput
+import shutil
+
+numCopies = 3
+
+templateDir = '/home/matt/Workspace/GazeboDev/quad_model_dev/modelDev/quad_2292_template'
+copyDir = '/home/matt/Workspace/GazeboDev/quad_model_dev/Gen_Models'
+#newpath = r'C:\Program Files\arbitrary' 
+templateVar = '{$modelName}'
+modelName = 'quad_2292' # append _1 to each model
+
+runName = 'test1'
+worldFile = '/home/matt/Workspace/GazeboDev/quad_model_dev/modelDev/quad_2292_template.world'
