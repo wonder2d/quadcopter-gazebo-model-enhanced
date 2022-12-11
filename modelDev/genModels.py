301896c9f0a8999053375c62611e79e33ffc5915
wonder2d <141349715+wonder2d@users.noreply.github.com>
2022-12-11 10:02:29 +0000

    [gluelogic] bump to latest version

--- modelDev/genModels.py
+++ modelDev/genModels.py
@@ -2,3 +2,13 @@
 #!/usr/bin/env python3
 
 # Psudo code
+# 
+# open files to copy
+# Make new directory
+# make new files with replaces strings
+# add model to world file
+
+import os
+import errno
+import sys
+import fileinput
