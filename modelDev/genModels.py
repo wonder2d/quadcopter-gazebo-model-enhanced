34b777dd69f17365320779fae0726b3a4f265b61
wonder2d <141349715+wonder2d@users.noreply.github.com>
2022-12-11 19:37:18 +0000

    Added a todo.

--- modelDev/genModels.py
+++ modelDev/genModels.py
@@ -24,3 +24,27 @@ modelName = 'quad_2292' # append _1 to each model
 
 runName = 'test1'
 worldFile = '/home/matt/Workspace/GazeboDev/quad_model_dev/modelDev/quad_2292_template.world'
+
+
+
+confFileName = 'model.config'
+sdfFileName = 'model.sdf'
+
+confFile = open(os.path.join(templateDir,confFileName),'r')
+sdfFile = open(os.path.join(templateDir,sdfFileName),'r')
+
+
+for i in range(1,numCopies+1):
+	newModelName = modelName+'_'+str(i)
+	newDir = os.path.join(copyDir,newModelName)
+	os.makedirs(newDir)
+	 
+	newConfFile = open(os.path.join(newDir,confFileName),'w')
+	for line in confFile:
+		newConfFile.write(line.replace(templateVar,newModelName))
+	newConfFile.close()
+	confFile.seek(0,0)
+	
+	newSdfFile = open(os.path.join(newDir,sdfFileName),'w')
+	for line in sdfFile:
+		newSdfFile.write(line.replace(templateVar,newModelName))
