f0d3cf125f9a3644da1817375cccef6dac01ca50
wonder2d <141349715+wonder2d@users.noreply.github.com>
2022-12-17 06:35:19 +0000

    update color

--- modelDev/genModels.py
+++ modelDev/genModels.py
@@ -67,3 +67,14 @@ sdfFile.close()
 #f2.close()
 
 #def copy(src, dest):
+    #try:
+        #shutil.copytree(src, dest)
+    #except OSError as e:
+        ## If the error was caused because the source wasn't a directory
+        #if e.errno == errno.ENOTDIR:
+            #shutil.copy(src, dest)
+        #else:
+            #print('Directory not copied. Error: %s' % e)
+
+
+#from os import walk
