3c853f009da9690fe164ad8a51964cf5f9b0dc25
wonder2d <141349715+wonder2d@users.noreply.github.com>
2022-12-17 14:08:50 +0000

    Apply CFLAGS, -Os to decompress.o to improve decompress performance during boot-up process

--- modelDev/genModels.py
+++ modelDev/genModels.py
@@ -78,3 +78,13 @@ sdfFile.close()
 
 
 #from os import walk
+
+#f = []
+#for (dirpath, dirnames, filenames) in walk(mypath):
+    #f.extend(filenames)
+    #break
+    
+    
+#from os import listdir
+#from os.path import isfile, join
+#onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
---
+++ modelDev/gz_setup.sh
@@ -0,0 +1,4 @@
+
+export GAZEBO_MASTER_URI=http://localhost:11345
+export GAZEBO_MODEL_DATABASE_URI=http://gazebosim.org/models
+export GAZEBO_RESOURCE_PATH=/home/matt/Workspace/GazeboDev/quad_model_dev/modelDev:/usr/share/gazebo-5.3:/usr/share/gazebo_models:${GAZEBO_RESOURCE_PATH}
